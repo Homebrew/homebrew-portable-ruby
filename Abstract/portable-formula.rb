69620228545ac5ea8e89b304bdf8d7bd1689d383
Xu Cheng <xucheng@me.com>
2016-04-20 22:11:52 +0800

    let's build staticly

--- Abstract/portable-formula.rb
+++ Abstract/portable-formula.rb
@@ -9,10 +9,6 @@ module PortableFormulaInstall
     end
 
     super
-
-    if OS.mac?
-      fix_dylibs!
-    end
   end
 end
 
@@ -34,61 +30,6 @@ class PortableFormula < Formula
       [Hardware::CPU.arch_64_bit]
     else
       [Hardware::CPU.arch_32_bit]
-    end
-  end
-
-  def dylibs_map
-    system_dylibs = {}
-    broken_dylibs = {}
-    brewed_dylibs = {}
-
-    Keg.new(prefix).find do |file|
-      next if file.symlink? || file.directory?
-      next unless file.dylib? || file.mach_o_executable? || file.mach_o_bundle?
-      file.dynamically_linked_libraries.each do |dylib|
-        next if dylib =~ /^@(loader_|executable_|r)path/
-
-        begin
-          Keg.for Pathname.new(dylib)
-        rescue NotAKegError
-          system_dylibs[file] ||= []
-          system_dylibs[file] << dylib
-        rescue Errno::ENOENT
-          broken_dylibs[file] ||= []
-          broken_dylibs[file] << dylib
-        else
-          brewed_dylibs[file] ||= []
-          brewed_dylibs[file] << dylib
-        end
-      end
-    end
-
-    [system_dylibs, broken_dylibs, brewed_dylibs]
-  end
-
-  def fix_dylibs!
-    _system_dylibs, broken_dylibs, brewed_dylibs = dylibs_map
-
-    unless broken_dylibs.empty?
-      error_msg = "Missing libraries found:\n"
-      error_msg += broken_dylibs.map do |file, dylibs|
-        " - From #{file.relative_path_from(prefix)}:\n" + \
-        dylibs.sort.map { |dylib| "   * #{dylib}" }.join("\n")
-      end.join("\n")
-      raise error_msg
-    end
-
-    brewed_dylibs.each do |file, dylibs|
-      dylibs.each do |dylib|
-        new_dylib = dylib.relative_path_from(file.dirname)
-        file.ensure_writable do
-          if file.dylib? || file.mach_o_bundle?
-            system "install_name_tool", "-change", dylib, "@loader_path/#{new_dylib}", file
-          elsif file.mach_o_executable?
-            system "install_name_tool", "-change", dylib, "@executable_path/#{new_dylib}", file
-          end
-        end
-      end
     end
   end
 end
