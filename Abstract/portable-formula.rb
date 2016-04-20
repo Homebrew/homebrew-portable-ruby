9b6efaddfcaab36cd9e39b02cf7cd401e62fb733
Xu Cheng <xucheng@me.com>
2016-04-20 16:37:27 +0800

    add portable-openssl

---
+++ Abstract/portable-formula.rb
@@ -0,0 +1,96 @@
+module PortableFormulaInstall
+  def install
+    if OS.mac? && OS::Mac.version > :leopard
+      opoo <<-EOS.undent
+        You are building portable Ruby on #{OS::Mac.version}.
+        As result, Ruby interpreter won't be able to work for OS X at lower version.
+        It's recommended to build this formula on OS X Leopard.
+      EOS
+    end
+
+    super
+
+    if OS.mac?
+      fix_dylibs!
+    end
+  end
+end
+
+class PortableFormula < Formula
+  def self.inherited(subclass)
+    subclass.class_eval do
+      keg_only "Portable formula is keg-only."
+
+      option "without-universal", "Don't build a universal binary" if OS.mac?
+
+      prepend PortableFormulaInstall
+    end
+  end
+
+  def archs
+    if build.with? "universal"
+      Hardware::CPU.universal_archs
+    elsif OS::Mac.prefer_64_bit?
+      [Hardware::CPU.arch_64_bit]
+    else
+      [Hardware::CPU.arch_32_bit]
+    end
+  end
+
+  def dylibs_map
+    system_dylibs = {}
+    broken_dylibs = {}
+    brewed_dylibs = {}
+
+    Keg.new(prefix).find do |file|
+      next if file.symlink? || file.directory?
+      next unless file.dylib? || file.mach_o_executable? || file.mach_o_bundle?
+      file.dynamically_linked_libraries.each do |dylib|
+        next if dylib =~ /^@(loader_|executable_|r)path/
+
+        begin
+          Keg.for Pathname.new(dylib)
+        rescue NotAKegError
+          system_dylibs[file] ||= []
+          system_dylibs[file] << dylib
+        rescue Errno::ENOENT
+          broken_dylibs[file] ||= []
+          broken_dylibs[file] << dylib
+        else
+          brewed_dylibs[file] ||= []
+          brewed_dylibs[file] << dylib
+        end
+      end
+    end
+
+    [system_dylibs, broken_dylibs, brewed_dylibs]
+  end
+
+  def fix_dylibs!
+    _system_dylibs, broken_dylibs, brewed_dylibs = dylibs_map
+
+    unless broken_dylibs.empty?
+      error_msg = "Missing libraries found:\n"
+      error_msg += broken_dylibs.map do |file, dylibs|
+        " - From #{file.relative_path_from(prefix)}:\n" + \
+        dylibs.sort.map { |dylib| "   * #{dylib}" }.join("\n")
+      end.join("\n")
+      raise error_msg
+    end
+
+    brewed_dylibs.each do |file, dylibs|
+      dylibs.each do |dylib|
+        new_dylib = lib/File.basename(dylib)
+        raise "Missing library: #{new_dylib}" unless new_dylib.file?
+        new_dylib = new_dylib.relative_path_from(file.dirname)
+        file.ensure_writable do
+          if file.dylib? || file.mach_o_bundle?
+            system "install_name_tool", "-change", dylib, "@loader_path/#{new_dylib}", file
+          elsif file.mach_o_executable?
+            system "install_name_tool", "-change", dylib, "@executable_path/#{new_dylib}", file
+          end
+        end
+      end
+    end
+  end
+end
---
+++ Formula/portable-openssl.rb
@@ -0,0 +1,130 @@
+require File.expand_path("../../Abstract/portable-formula", __FILE__)
+
+class PortableOpenssl < PortableFormula
+  desc "Portable OpenSSL"
+  homepage "https://openssl.org/"
+  url "https://www.openssl.org/source/openssl-1.0.2g.tar.gz"
+  mirror "https://dl.bintray.com/homebrew/mirror/openssl-1.0.2g.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2g.tar.gz"
+  sha256 "b784b1b3907ce39abf4098702dade6365522a253ad1552e267a9a0e89594aa33"
+
+  depends_on "makedepend" => :build
+
+  # Replace with upstream url if they merge the more robust fix
+  # https://github.com/openssl/openssl/pull/597
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/patches/3f1dc8ea145a70543aded8101a0c725abf82fc45/openssl/revert-pass-pure-constants-verbatim.patch"
+    sha256 "e38f84181a56e70028ade8408ad70aaffaea386b7e1b35de55728ae878d544aa"
+  end
+
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/patches/3f1dc8ea145a70543aded8101a0c725abf82fc45/openssl/tshort-asm.patch"
+    sha256 "f161e2fc1395efcb53d785004d67d4962d28aa8ce282a91020f12809c03b2afd"
+  end
+
+  resource "cacert" do
+    # homepage "http://curl.haxx.se/docs/caextract.html", "https://github.com/bagder/ca-bundle"
+    url "https://raw.githubusercontent.com/bagder/ca-bundle/bff056d04b9e2c92ea8c83b2e39be9c8d0501039/ca-bundle.crt"
+    sha256 "0f119da204025da7808273fab42ed8e030cafb5c7ea4e1deda4e75f066f528fb"
+  end
+
+  def openssldir
+    libexec/"etc/openssl"
+  end
+
+  def arch_args
+    if OS.mac?
+      {
+        :x86_64 => %w[darwin64-x86_64-cc enable-ec_nistp_64_gcc_128],
+        :i386   => %w[darwin-i386-cc],
+      }
+    else
+      {
+        :x86_64 => %w[linux-x86_64],
+        :i386  => %w[linux-generic32],
+      }
+    end
+  end
+
+  def configure_args
+    %W[
+      --prefix=#{prefix}
+      --openssldir=#{openssldir}
+      no-ssl2
+      zlib-dynamic
+      shared
+      enable-cms
+    ]
+  end
+
+  def install
+    # Load zlib from an explicit path instead of relying on dyld's fallback
+    # path, which is empty in a SIP context. This patch will be unnecessary
+    # when we begin building openssl with no-comp to disable TLS compression.
+    # https://langui.sh/2015/11/27/sip-and-dlopen
+    inreplace "crypto/comp/c_zlib.c",
+              'zlib_dso = DSO_load(NULL, "z", NULL, 0);',
+              'zlib_dso = DSO_load(NULL, "/usr/lib/libz.dylib", NULL, DSO_FLAG_NO_NAME_TRANSLATION);'
+
+    ENV.permit_arch_flags if build.with? "universal"
+
+    dirs = []
+
+    archs.each do |arch|
+      if build.with? "universal"
+        dir = "build-#{arch}"
+        dirs << dir
+        mkdir dir
+        mkdir "#{dir}/engines"
+        system "make", "clean"
+      end
+
+      ENV.deparallelize
+      system "perl", "./Configure", *(configure_args + arch_args[arch])
+      system "make", "depend"
+      system "make"
+      system "make", "test"
+
+      if build.with? "universal"
+        cp "include/openssl/opensslconf.h", dir
+        cp Dir["*.?.?.?.dylib", "*.a", "apps/openssl"], dir
+        cp Dir["engines/**/*.dylib"], "#{dir}/engines"
+      end
+    end
+
+    system "make", "install", "MANDIR=#{buildpath}/stage/man", "MANSUFFIX=ssl"
+
+    if build.with? "universal"
+      %w[libcrypto libssl].each do |libname|
+        system "lipo", "-create", "#{dirs.first}/#{libname}.1.0.0.dylib",
+                                  "#{dirs.last}/#{libname}.1.0.0.dylib",
+                       "-output", "#{lib}/#{libname}.1.0.0.dylib"
+        system "lipo", "-create", "#{dirs.first}/#{libname}.a",
+                                  "#{dirs.last}/#{libname}.a",
+                       "-output", "#{lib}/#{libname}.a"
+      end
+
+      Dir.glob("#{dirs.first}/engines/*.dylib") do |engine|
+        libname = File.basename(engine)
+        system "lipo", "-create", "#{dirs.first}/engines/#{libname}",
+                                  "#{dirs.last}/engines/#{libname}",
+                       "-output", "#{lib}/engines/#{libname}"
+      end
+
+      system "lipo", "-create", "#{dirs.first}/openssl",
+                                "#{dirs.last}/openssl",
+                     "-output", "#{bin}/openssl"
+
+      confs = archs.map do |arch|
+        <<-EOS.undent
+          #ifdef __#{arch}__
+          #{(buildpath/"build-#{arch}/opensslconf.h").read}
+          #endif
+          EOS
+      end
+      (include/"openssl/opensslconf.h").atomic_write confs.join("\n")
+    end
+
+    openssldir.install resource("cacert").files("ca-bundle.crt" => "cert.pem")
+  end
+end
