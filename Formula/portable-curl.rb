53998df748b87b46ce9d89cd81b1783b90c84227
Xu Cheng <xucheng@me.com>
2016-05-06 19:25:05 +0800

    portable-curl: fix build for linux

--- Formula/portable-curl.rb
+++ Formula/portable-curl.rb
@@ -14,7 +14,7 @@ class PortableCurl < PortableFormula
   #      https://curl.haxx.se/mail/lib-2011-12/0093.html
   def install
     ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["portable-openssl"].opt_prefix}/lib/pkgconfig"
-    ENV["LIBS"] = "-Wl,-search_paths_first -ldl" if OS.linux?
+    ENV["LIBS"] = `pkg-config openssl --static --libs`.chomp if OS.linux?
     args = %W[
       --disable-debug
       --disable-dependency-tracking
