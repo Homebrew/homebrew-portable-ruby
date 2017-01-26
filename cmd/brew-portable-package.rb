abc51a95e1b85119fa8067c7cb2dcfd8e4ed19fa
Alyssa Ross <hi@alyssa.is>
2017-01-26 17:44:48 +0000

    portable-package: fix another Linux typo

--- cmd/brew-portable-package.rb
+++ cmd/brew-portable-package.rb
@@ -20,7 +20,7 @@ ARGV.named.each do |name|
   safe_system "brew", "test", name
   puts "Library linkage:"
   if OS.linux?
-    puts Utils.open_read "ldd", "#{f.bin}/#{name.gsub(/^portable-/, "")}"
+    puts Utils.popen_read "ldd", "#{f.bin}/#{name.gsub(/^portable-/, "")}"
   else
     puts Utils.popen_read "brew", "linkage", name
   end
