d9abad4a211b32d166a8fd12b9123175cba2d2ff
Xu Cheng <git@xuc.me>
2017-04-08 14:53:43 +0800

    portable-libyaml 0.1.7
    Also add test

--- Formula/portable-libyaml.rb
+++ Formula/portable-libyaml.rb
@@ -3,16 +3,9 @@ require File.expand_path("../../Abstract/portable-formula", __FILE__)
 class PortableLibyaml < PortableFormula
   desc "Portable libyaml"
   homepage "http://pyyaml.org/wiki/LibYAML"
-  url "http://pyyaml.org/download/libyaml/yaml-0.1.6.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/liby/libyaml/libyaml_0.1.6.orig.tar.gz"
-  sha256 "7da6971b4bd08a986dd2a61353bc422362bd0edcc67d7ebaac68c95f74182749"
-
-  # address CVE-2014-9130
-  # https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2014-9130
-  patch do
-    url "https://bitbucket.org/xi/libyaml/commits/2b9156756423e967cfd09a61d125d883fca6f4f2/raw/"
-    sha256 "30546a280c4f9764a93ff5f4f88671a02222e9886e7f63ee19aebf1b2086a7fe"
-  end
+  url "http://pyyaml.org/download/libyaml/yaml-0.1.7.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/liby/libyaml/libyaml_0.1.7.orig.tar.gz"
+  sha256 "8088e457264a98ba451a90b8661fcb4f9d6f478f7265d48322a196cec2480729"
 
   def install
     ENV.universal_binary if build.with? "universal"
@@ -21,5 +14,21 @@ class PortableLibyaml < PortableFormula
                           "--enable-static",
                           "--disable-shared"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <yaml.h>
+
+      int main()
+      {
+        yaml_parser_t parser;
+        yaml_parser_initialize(&parser);
+        yaml_parser_delete(&parser);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lyaml", "-o", "test"
+    system "./test"
   end
 end
