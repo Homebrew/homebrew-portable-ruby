16f3bd3585917161d43c7ea3fbb4512916dc4586
Xu Cheng <git@xuc.me>
2017-04-08 14:41:35 +0800

    portable-expat 2.2.0
    Also add test

--- Formula/portable-expat.rb
+++ Formula/portable-expat.rb
@@ -3,8 +3,9 @@ require File.expand_path("../../Abstract/portable-formula", __FILE__)
 class PortableExpat < PortableFormula
   desc "Portable expat"
   homepage "http://www.libexpat.org"
-  url "https://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz"
-  sha256 "823705472f816df21c8f6aa026dd162b280806838bb55b3432b0fb1fcca7eb86"
+  url "https://downloads.sourceforge.net/project/expat/expat/2.2.0/expat-2.2.0.tar.bz2"
+  mirror "https://fossies.org/linux/www/expat-2.2.0.tar.bz2"
+  sha256 "d9e50ff2d19b3538bd2127902a89987474e1a4db8e43a66a4d1a712ab9a504ff"
 
   def install
     ENV.universal_binary if build.with? "universal"
@@ -13,5 +14,51 @@ class PortableExpat < PortableFormula
                           "--enable-static",
                           "--disable-shared"
     system "make", "install"
+  end
+
+  test do
+    cp_r Dir["#{prefix}/*"], testpath
+    (testpath/"test.xml").write <<-EOS.undent
+      <?xml version="1.0" standalone="yes"?>
+      <str>Hello, world!</str>
+    EOS
+    system testpath/"bin/xmlwf", "test.xml"
+
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include "expat.h"
+
+      static void XMLCALL my_StartElementHandler(
+        void *userdata,
+        const XML_Char *name,
+        const XML_Char **atts)
+      {
+        printf("tag:%s|", name);
+      }
+
+      static void XMLCALL my_CharacterDataHandler(
+        void *userdata,
+        const XML_Char *s,
+        int len)
+      {
+        printf("data:%.*s|", len, s);
+      }
+
+      int main()
+      {
+        static const char str[] = "<str>Hello, world!</str>";
+        int result;
+
+        XML_Parser parser = XML_ParserCreate("utf-8");
+        XML_SetElementHandler(parser, my_StartElementHandler, NULL);
+        XML_SetCharacterDataHandler(parser, my_CharacterDataHandler);
+        result = XML_Parse(parser, str, sizeof(str), 1);
+        XML_ParserFree(parser);
+
+        return result;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lexpat", "-o", "test"
+    assert_equal "tag:str|data:Hello, world!|", shell_output("./test")
   end
 end
