dd45df1d64d80fc61559fee9d8e3ce3b4376ccda
Mike McQuaid <mike@mikemcquaid.com>
2018-05-03 08:35:09 +0100

    portable-berkeley-db: portable fixes.

--- Formula/portable-berkeley-db.rb
+++ Formula/portable-berkeley-db.rb
@@ -1,22 +1,13 @@
-class PortableBerkeleyDb < Formula
+class PortableBerkeleyDb < PortableFormula
   desc "High performance key/value database"
   homepage "https://www.oracle.com/technology/products/berkeley-db/index.html"
   url "https://download.oracle.com/berkeley-db/db-6.2.32.tar.gz"
   sha256 "a9c5e2b004a5777aa03510cfe5cd766a4a3b777713406b02809c17c8e0e7a8fb"
 
-  bottle do
-    cellar :any
-    sha256 "ee2f38644137df02f3ebd9fa689a09f1e12c2a21dab38a03f77e2c213a6135ef" => :high_sierra
-    sha256 "eb54f8ab1d0149e073f641ad066e6ffb179afdee83cd3211e90eeaaaa4a7bc9a" => :sierra
-    sha256 "c084857cfdd9bbc5eec028cc551d2323c050489e8a963ec40076270db8d14fb3" => :el_capitan
-    sha256 "1ed9a471cd8a38b58053a86e37b4ec83c09944781158fa20e2ba300f3b374f4f" => :yosemite
-  end
-
-  depends_on :java => [:optional, :build]
-
   def install
     # BerkeleyDB dislikes parallel builds
     ENV.deparallelize
+    
     # --enable-compat185 is necessary because our build shadows
     # the system berkeley db 1.x
     args = %W[
@@ -31,7 +22,6 @@ class PortableBerkeleyDb < Formula
       --enable-dbm
       --enable-stl
     ]
-    args << "--enable-java" if build.with? "java"
 
     # BerkeleyDB requires you to build everything from the build_unix subdirectory
     cd "build_unix" do
