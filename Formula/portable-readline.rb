672b280ea83c70bc3c8582d97705e3be51764f80
Bo Anderson <mail@boanderson.me>
2021-09-27 01:18:14 +0100

    portable-readline 8.1.1

--- Formula/portable-readline.rb
+++ Formula/portable-readline.rb
@@ -3,20 +3,18 @@ require File.expand_path("../Abstract/portable-formula", __dir__)
 class PortableReadline < PortableFormula
   desc "Library for command-line editing"
   homepage "https://tiswww.case.edu/php/chet/readline/rltop.html"
-  url "https://ftpmirror.gnu.org/readline/readline-8.0.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/readline/readline-8.0.tar.gz"
-  version "8.0.4"
-  sha256 "e339f51971478d369f8a053a330a190781acb9864cf4c541060f12078948e461"
+  url "https://ftp.gnu.org/gnu/readline/readline-8.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/readline/readline-8.1.tar.gz"
+  version "8.1.1"
+  sha256 "f8ceb4ee131e3232226a17f51b164afc46cd0b9e6cef344be87c65962cb82b02"
+  license "GPL-3.0-or-later"
 
   %w[
-    001 d8e5e98933cf5756f862243c0601cb69d3667bb33f2c7b751fe4e40b2c3fd069
-    002 36b0febff1e560091ae7476026921f31b6d1dd4c918dcb7b741aa2dad1aec8f7
-    003 94ddb2210b71eb5389c7756865d60e343666dfb722c85892f8226b26bb3eeaef
-    004 b1aa3d2a40eee2dea9708229740742e649c32bb8db13535ea78f8ac15377394c
+    001 682a465a68633650565c43d59f0b8cdf149c13a874682d3c20cb4af6709b9144
   ].each_slice(2) do |p, checksum|
     patch :p0 do
-      url "https://ftp.gnu.org/gnu/readline/readline-8.0-patches/readline80-#{p}"
-      mirror "https://ftpmirror.gnu.org/readline/readline-8.0-patches/readline80-#{p}"
+      url "https://ftp.gnu.org/gnu/readline/readline-8.1-patches/readline81-#{p}"
+      mirror "https://ftpmirror.gnu.org/readline/readline-8.1-patches/readline81-#{p}"
       sha256 checksum
     end
   end
@@ -25,7 +23,6 @@ class PortableReadline < PortableFormula
 
   def install
     system "./configure", "--prefix=#{prefix}",
-                          "--enable-multibyte",
                           "--enable-static",
                           "--disable-shared",
                           ("--with-curses" if OS.linux?)
