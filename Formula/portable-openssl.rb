b795a491e0688d1dc20b84af204194c81cbf61ad
Bo Anderson <mail@boanderson.me>
2022-04-16 19:00:05 +0100

    portable-openssl 1.1.1n

--- Formula/portable-openssl.rb
+++ Formula/portable-openssl.rb
@@ -3,30 +3,16 @@ require File.expand_path("../Abstract/portable-formula", __dir__)
 class PortableOpenssl < PortableFormula
   desc "SSL/TLS cryptography library"
   homepage "https://openssl.org/"
-  url "https://www.openssl.org/source/openssl-1.1.1l.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.1l.tar.gz"
-  mirror "https://www.openssl.org/source/old/1.1.1/openssl-1.1.1l.tar.gz"
-  sha256 "0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1"
+  url "https://www.openssl.org/source/openssl-1.1.1n.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.1n.tar.gz"
+  mirror "https://www.openssl.org/source/old/1.1.1/openssl-1.1.1n.tar.gz"
+  sha256 "40dceb51a4f6a5275bde0e6bf20ef4b91bfc32ed57c0552e2e8e15463372b17a"
   license "OpenSSL"
 
   resource "cacert" do
     # https://curl.se/docs/caextract.html
-    url "https://curl.se/ca/cacert-2021-09-30.pem"
-    sha256 "f524fc21859b776e18df01a87880efa198112214e13494275dbcbd9bcb71d976"
-  end
-
-  # Fix build on older macOS versions.
-  # Remove with the next version.
-  patch do
-    url "https://github.com/openssl/openssl/commit/96ac8f13f4d0ee96baf5724d9f96c44c34b8606c.patch?full_index=1"
-    sha256 "dd5498c0910c0ae91738fe8e796f4deb4767b08217c1a859fe390147f24809c6"
-  end
-
-  # Fix build on older macOS versions.
-  # Remove with the next version.
-  patch do
-    url "https://github.com/openssl/openssl/commit/2f3b120401533db82e99ed28de5fc8aab1b76b33.patch?full_index=1"
-    sha256 "a66dcd4a3a291858deefaf260ffd8f2f55da953724e7a14db9c4523d8b7ef383"
+    url "https://curl.se/ca/cacert-2022-03-29.pem"
+    sha256 "1979e7fe618c51ed1c9df43bba92f977a0d3fe7497ffa2a5e80dfc559a1e5a29"
   end
 
   def openssldir
@@ -64,8 +50,7 @@ class PortableOpenssl < PortableFormula
     system "make"
     system "make", "test"
 
-    system "make", "install", "MANDIR=#{man}"
-    rm_rf man
+    system "make", "install_sw"
 
     # Ruby doesn't support passing --static to pkg-config.
     # Unfortunately, this means we need to modify the OpenSSL pc file.
