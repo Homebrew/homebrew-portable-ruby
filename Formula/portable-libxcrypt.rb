01ad018752bf63423d8570528f53bf151b67d09a
Bo Anderson <mail@boanderson.me>
2022-04-16 04:55:21 +0100

    portable-libxcrypt 4.4.28 (new formula)

---
+++ Formula/portable-libxcrypt.rb
@@ -0,0 +1,54 @@
+require File.expand_path("../Abstract/portable-formula", __dir__)
+
+class PortableLibxcrypt < PortableFormula
+  desc "Extended crypt library for descrypt, md5crypt, bcrypt, and others"
+  homepage "https://github.com/besser82/libxcrypt"
+  url "https://github.com/besser82/libxcrypt/releases/download/v4.4.28/libxcrypt-4.4.28.tar.xz"
+  sha256 "9e936811f9fad11dbca33ca19bd97c55c52eb3ca15901f27ade046cc79e69e87"
+  license "LGPL-2.1-or-later"
+
+  def install
+    # Compling util-xbzero.c crashes with "invalid argument to gimple call" on Wheezy's GCC 4.7.2.
+    # Use the GCC 4.9 backport to avoid this.
+    ENV["HOMEBREW_CC"] = "/usr/lib/gcc-4.9-backport/bin/gcc" if ENV["HOMEBREW_ON_DEBIAN7"].present?
+    system "./configure", *std_configure_args,
+                          "--enable-static",
+                          "--disable-shared",
+                          "--disable-obsolete-api",
+                          "--disable-xcrypt-compat-files",
+                          "--disable-failure-tokens",
+                          "--disable-valgrind"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <crypt.h>
+      #include <errno.h>
+      #include <stdio.h>
+      #include <string.h>
+
+      int main()
+      {
+        char *hash = crypt("abc", "$2b$05$abcdefghijklmnopqrstuu");
+
+        if (errno) {
+          fprintf(stderr, "Received error: %s", strerror(errno));
+          return errno;
+        }
+        if (hash == NULL) {
+          fprintf(stderr, "Hash is NULL");
+          return -1;
+        }
+        if (strcmp(hash, "$2b$05$abcdefghijklmnopqrstuuRWUgMyyCUnsDr8evYotXg5ZXVF/HhzS")) {
+          fprintf(stderr, "Unexpected hash output");
+          return -1;
+        }
+
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lcrypt", "-o", "test"
+    system "./test"
+  end
+end
