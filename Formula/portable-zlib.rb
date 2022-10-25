8d4c6ea973543633e638607499b03ce4d52463f0
Bo Anderson <mail@boanderson.me>
2022-10-25 13:08:59 +0100

    portable-zlib 1.2.13

--- Formula/portable-zlib.rb
+++ Formula/portable-zlib.rb
@@ -3,9 +3,10 @@ require File.expand_path("../Abstract/portable-formula", __dir__)
 class PortableZlib < PortableFormula
   desc "General-purpose lossless data-compression library"
   homepage "https://www.zlib.net/"
-  url "https://zlib.net/zlib-1.2.12.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/libpng/zlib/1.2.12/zlib-1.2.12.tar.gz"
-  sha256 "91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
+  url "https://zlib.net/zlib-1.2.13.tar.gz"
+  mirror "https://fossies.org/linux/misc/zlib-1.2.13.tar.gz"
+  mirror "https://fossies.org/linux/misc/legacy/zlib-1.2.13.tar.gz"
+  sha256 "b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
   license "Zlib"
 
   # https://zlib.net/zlib_how.html
@@ -13,20 +14,6 @@ class PortableZlib < PortableFormula
     url "https://zlib.net/zpipe.c"
     version "20051211"
     sha256 "68140a82582ede938159630bca0fb13a93b4bf1cb2e85b08943c26242cf8f3a6"
-  end
-
-  # Patch for configure issue
-  # Remove with the next release
-  patch do
-    url "https://github.com/madler/zlib/commit/05796d3d8d5546cf1b4dfe2cd72ab746afae505d.patch?full_index=1"
-    sha256 "68573842f1619bb8de1fa92071e38e6e51b8df71371e139e4e96be19dd7e9694"
-  end
-
-  # Patch for CRC compatibility issue
-  # Remove with the next release
-  patch do
-    url "https://github.com/madler/zlib/commit/ec3df00224d4b396e2ac6586ab5d25f673caa4c2.patch?full_index=1"
-    sha256 "c7d1cbb58b144c48b7fa8b52c57531e9fd80ab7d87c5d58ba76a9d33c12cb047"
   end
 
   def install
