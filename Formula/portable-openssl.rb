0cbb07a840ececf96757f4f59ef96bce64c20dc9
Bo Anderson <mail@boanderson.me>
2022-10-25 13:08:46 +0100

    portable-openssl 1.1.1q

--- Formula/portable-openssl.rb
+++ Formula/portable-openssl.rb
@@ -3,28 +3,16 @@ require File.expand_path("../Abstract/portable-formula", __dir__)
 class PortableOpenssl < PortableFormula
   desc "SSL/TLS cryptography library"
   homepage "https://openssl.org/"
-  url "https://www.openssl.org/source/openssl-1.1.1o.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.1o.tar.gz"
-  mirror "https://www.openssl.org/source/old/1.1.1/openssl-1.1.1o.tar.gz"
-  sha256 "9384a2b0570dd80358841464677115df785edb941c71211f75076d72fe6b438f"
+  url "https://www.openssl.org/source/openssl-1.1.1q.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.1q.tar.gz"
+  mirror "https://www.openssl.org/source/old/1.1.1/openssl-1.1.1q.tar.gz"
+  sha256 "d7939ce614029cdff0b6c20f0e2e5703158a489a72b2507b8bd51bf8c8fd10ca"
   license "OpenSSL"
 
   resource "cacert" do
     # https://curl.se/docs/caextract.html
-    url "https://curl.se/ca/cacert-2022-03-29.pem"
-    sha256 "1979e7fe618c51ed1c9df43bba92f977a0d3fe7497ffa2a5e80dfc559a1e5a29"
-  end
-
-  # Fix failing test due to expired certificates.
-  # Remove with the next version (1.1.1p).
-  patch do
-    url "https://github.com/openssl/openssl/commit/73db5d82489b3ec09ccc772dfcee14fef0e8e908.patch?full_index=1"
-    sha256 "4b04ce0b7a3132c640bdc7726c7efaeb28572c5b8cdffcdc80fea700ded964e3"
-  end
-
-  patch do
-    url "https://github.com/openssl/openssl/commit/b7ce611887cfac633aacc052b2e71a7f195418b8.patch?full_index=1"
-    sha256 "6a81f4b2edb9ca3d56d897b4c85faac59fb488434dae6f0b5c525e9f96c879df"
+    url "https://curl.se/ca/cacert-2022-10-11.pem"
+    sha256 "2cff03f9efdaf52626bd1b451d700605dc1ea000c5da56bd0fc59f8f43071040"
   end
 
   def openssldir
