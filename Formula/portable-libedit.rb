636d81949252511f33daa4d8cb18816924bd0283
Bo Anderson <mail@boanderson.me>
2023-09-29 15:18:57 +0100

    portable-libedit 20230828-3.1

--- Formula/portable-libedit.rb
+++ Formula/portable-libedit.rb
@@ -3,9 +3,9 @@ require File.expand_path("../Abstract/portable-formula", __dir__)
 class PortableLibedit < PortableFormula
   desc "BSD-style licensed readline alternative"
   homepage "https://thrysoee.dk/editline/"
-  url "https://thrysoee.dk/editline/libedit-20221030-3.1.tar.gz"
-  version "20221030-3.1"
-  sha256 "f0925a5adf4b1bf116ee19766b7daa766917aec198747943b1c4edf67a4be2bb"
+  url "https://thrysoee.dk/editline/libedit-20230828-3.1.tar.gz"
+  version "20230828-3.1"
+  sha256 "4ee8182b6e569290e7d1f44f0f78dac8716b35f656b76528f699c69c98814dad"
   license "BSD-3-Clause"
 
   on_linux do
