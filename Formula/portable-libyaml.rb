c350667458ed3ce14ca3ad1c17cee6dfb5ceab8e
Sam Ford <1584702+samford@users.noreply.github.com>
2024-05-28 07:52:10 -0400

    portable-libyaml: add livecheck

--- Formula/portable-libyaml.rb
+++ Formula/portable-libyaml.rb
@@ -7,6 +7,11 @@ class PortableLibyaml < PortableFormula
   sha256 "c642ae9b75fee120b2d96c712538bd2cf283228d2337df2cf2988e3c02678ef4"
   license "MIT"
 
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
   def install
     system "./configure", *portable_configure_args,
                           "--disable-dependency-tracking",
