3dc48b04598b3af0b395dbe1939bd648d23477fb
Patrick Linnane <patrick@linnane.io>
2024-12-25 18:53:59 +0000

    portable-ruby 3.4.1

--- Formula/portable-ruby.rb
+++ Formula/portable-ruby.rb
@@ -3,8 +3,8 @@ require File.expand_path("../Abstract/portable-formula", __dir__)
 class PortableRuby < PortableFormula
   desc "Powerful, clean, object-oriented scripting language"
   homepage "https://www.ruby-lang.org/"
-  url "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.6.tar.gz"
-  sha256 "8dc48fffaf270f86f1019053f28e51e4da4cce32a36760a0603a9aee67d7fd8d"
+  url "https://cache.ruby-lang.org/pub/ruby/3.4/ruby-3.4.1.tar.gz"
+  sha256 "3d385e5d22d368b064c817a13ed8e3cc3f71a7705d7ed1bae78013c33aa7c87f"
   license "Ruby"
 
   # This regex restricts matching to versions other than X.Y.0.
