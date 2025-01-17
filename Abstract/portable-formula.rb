8041e8794510d22f8760c1e9ecbf87cbf32cddc8
Bo Anderson <mail@boanderson.me>
2025-01-17 04:10:26 +0000

    Abstract/portable-formula: use glibc@2.17 on arm64

--- Abstract/portable-formula.rb
+++ Abstract/portable-formula.rb
@@ -84,10 +84,11 @@ class PortableFormula < Formula
       on_linux do
         on_intel do
           depends_on "glibc@2.13" => :build
-          depends_on "linux-headers@4.4" => :build
         end
-        # When we move from Ubuntu 22.04, on ARM we should add a dependency to glibc@2.35 and linux-headers@5.15.
-        # When doing so, remember to update the C++ Intel conditional in the portable-ruby formula.
+        on_arm do
+          depends_on "glibc@2.17" => :build
+        end
+        depends_on "linux-headers@4.4" => :build
       end
 
       prepend PortableFormulaMixin
